eb162717618a08358dfa245cdc20fefb73ef07b9
Daniel Gallagher <danielgallagher@email.arizona.edu>
2021-01-30 00:33:43 -0700

    Preliminary PlutoSDR support

--- scripts/setup-gnuradio.sh
+++ scripts/setup-gnuradio.sh
@@ -8,6 +8,57 @@ export PATH="$HOME/.local/bin:${PATH}"
 cd
 sudo mv 90-usrp.conf /etc/sysctl.d/
 
+### PYBOMBS
+# sudo pip3 install pybombs
+# pybombs auto-config
+# pybombs recipes add-defaults
+### PlutoSDR Support
+sudo apt install libxml2 libxml2-dev bison flex cmake git libaio-dev libboost-all-dev libgmp-dev
+sudo apt install doxygen
+sudo apt install libusb-1.0-0-dev
+sudo apt install libavahi-common-dev libavahi-client-dev
+sudo apt install swig
+sudo apt install liborc-dev
+
+### LIBIIO
+cd /home/gnuradio/src
+git clone https://github.com/pcercuei/libini.git
+cd libini
+mkdir build && cd build && cmake ../ && make && sudo make install
+
+cd ..
+git clone https://github.com/analogdevicesinc/libiio.git
+cd libiio
+cmake . 
+make 
+sudo make install
+cd ..
+sudo mv 53-adi-plutosdr-usb.rules /etc/udev/rules.d/
+
+### LIBAD9361
+cd /home/gnuradio/src
+git clone https://github.com/analogdevicesinc/libad9361-iio.git
+cd libad9361-iio
+cmake .
+make 
+sudo make install
+cd ..
+
+### IIO OSCILLOSCOPE
+sudo apt install libglib2.0-dev libgtk2.0-dev libgtkdatabox-dev libmatio-dev libfftw3-dev libxml2 libxml2-dev bison flex libavahi-common-dev libavahi-client-dev libcurl4-openssl-dev libjansson-dev cmake libaio-dev
+cd /home/gnuradio/src
+
+cd ..
+git clone https://github.com/analogdevicesinc/iio-oscilloscope.git
+cd iio-oscilloscope
+cd iio-oscilloscope
+git checkout origin/master
+mkdir build && cd build
+cmake ../ && make -j $(nproc)
+sudo make install
+### ADI TOOLS
+git clone https://github.com/analogdevicesinc/plutosdr_scripts.git ~/.adi/plutosdr_scripts
+git clone https://github.com/analogdevicesinc/linux_image_ADI-scripts.git ~/.adi/linux_scripts
 cd
 echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib' >> .profile
 echo 'export PYTHONPATH=${PYTHONPATH}:/usr/local/lib/python3/dist-packages' >> .profile
@@ -39,6 +90,16 @@ sudo apt-get update
 sudo apt-get -y install gr-fcdproplus gr-fosphor gr-iqbal gr-limesdr gr-osmosdr
 
 sudo apt-get -y install gqrx-sdr inspectrum
+### GR IIO
+cd /home/gnuradio/src
+git clone -b upgrade-3.8 https://github.com/analogdevicesinc/gr-iio.git
+cd gr-iio
+cmake .
+make 
+sudo make install
+cd ..
+sudo ldconfig
+cd
 
 sudo snap install urh
 
@@ -62,6 +123,7 @@ xdg-icon-resource install --context apps --novendor --size 96 Pictures/inspectru
 xdg-icon-resource install --context apps --novendor --size 96 Pictures/fosphor-icon.png
 xdg-icon-resource install --context apps --novendor --size 96 Pictures/urhpng.png
 
+### CLEAN UP OUR STUFF
 rm -rf Downloads/*
 
 ### FAVORITE APPLICATIONS
